00e26e5f73738e22ee3bcd86110d9096ba2c5fec
jasmin285 <56223174+jasmin285@users.noreply.github.com>
2020-02-20 02:18:38 +0200

    Update lcd.h

--- ARM-TM4C123/LCD_Driver/lcd.h
+++ ARM-TM4C123/LCD_Driver/lcd.h
@@ -2,6 +2,17 @@
 #include "gpio.h"
 #include "stdio.h"
 
+#define CLEAR 				0x01
+#define RETURN_CURSOR 			0x02
+#define SHIFT_CURSOR_RIGHT 		0x06
+#define SHIFT_CURSOR_LEFT		0x04
+#define DISPLAY_OFF_CURSOR_OFF          0x08
+#define DISPLAY_BLINK_CURSOR_ON	        0x0F
+#define FUNC_SET_8BIT_1LINE             0x30
+#define FUNC_SET_8BIT_2LINE             0x38
+#define CURSOR_BEGIN_FIRST_LINE		0x80
+#define CURSOR_BEGIN_SECOND_LINE	0xC0
+
 void delayMs(int n);
 void delayUs(int n);
 void LCD_init(void);

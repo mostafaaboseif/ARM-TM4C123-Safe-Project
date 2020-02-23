7595906e90b9056c62f96940cf877f909107c0de
alien19 <engmariaam@outlook.com>
2020-02-23 15:53:57 +0000

    Editing naming convention and Adding more functions
    Co-Authored-By: mostafaaboseif <mostafaaboseif@users.noreply.github.com>
    Co-Authored-By: mgtm98 <mgtm98@users.noreply.github.com>

--- ARM-TM4C123/GPIO_Driver/gpio.c
+++ ARM-TM4C123/GPIO_Driver/gpio.c
@@ -1,87 +1,41 @@
 #include "gpio.h"
 
-void gpio_init_port(volatile uint32_t portAdress, PinDir dir){
-	switch(portAdress){
-		case PORTA_AHB:
-		case PORTA_APB:
-			(*SYSCTL_RCGCGPIO) |= 0x01;
-			while(!((*SYSCTL_PRGPIO)|= 0x01));
-			break;
-		
-		case PORTB_AHB:
-		case PORTB_APB:
-			(*SYSCTL_RCGCGPIO) |= 0x02;
-			while(!((*SYSCTL_PRGPIO)|= 0x02));
-			break;
-		
-		case PORTC_AHB:
-		case PORTC_APB:
-			(*SYSCTL_RCGCGPIO) |= 0x04;
-			while(!((*SYSCTL_PRGPIO)|= 0x04));
-			break;
-		
-		case PORTD_AHB:
-		case PORTD_APB:
-			(*SYSCTL_RCGCGPIO) |= 0x08;
-			while(!((*SYSCTL_PRGPIO)|= 0x08));
-			break;
-		
-		case PORTE_AHB:
-		case PORTE_APB:
-			(*SYSCTL_RCGCGPIO) |= 0x10;
-			while(!((*SYSCTL_PRGPIO)|= 0x10));
-			break;
-		
-		case PORTF_AHB:
-		case PORTF_APB:
-			(*SYSCTL_RCGCGPIO) |= 0x20;
-			while(!((*SYSCTL_PRGPIO)|= 0x20));
-			break;
-	}
-	Memory(portAdress, GPIO_LCK) 		= UNLOCK;
-	Memory(portAdress, GPIO_CR) 		= 0xFF;
-	Memory(portAdress, GPIO_DEN) 		= 0xFF;
-	Memory(portAdress, GPIO_AMSEL)	= 0x00;
-	Memory(portAdress, GPIO_AFSEL) 	= 0x00;
-	Memory(portAdress, GPIO_PCTL) 	= 0x00;
-	Memory(portAdress, GPIO_DIR) 		= dir;
-}
+void (*callbackTable[6][8])();
 
-
-void gpio_init_pin(volatile uint32_t portAddress, Pin pin, PinMode mode, PinDir dir){
+void GPIO_initPin(volatile uint32_t portAddress, Pin pin, PinMode mode, PinDir dir){
 	switch(portAddress){
 		case PORTA_AHB:
-		case PORTA_APB:
+		case PORTA:
 			(*SYSCTL_RCGCGPIO) |= 0x01;
 			while(!((*SYSCTL_PRGPIO)|= 0x01));
 			break;
 		
 		case PORTB_AHB:
-		case PORTB_APB:
+		case PORTB:
 			(*SYSCTL_RCGCGPIO) |= 0x02;
 			while(!((*SYSCTL_PRGPIO)|= 0x02));
 			break;
 		
 		case PORTC_AHB:
-		case PORTC_APB:
+		case PORTC:
 			(*SYSCTL_RCGCGPIO) |= 0x04;
 			while(!((*SYSCTL_PRGPIO)|= 0x04));
 			break;
 		
 		case PORTD_AHB:
-		case PORTD_APB:
+		case PORTD:
 			(*SYSCTL_RCGCGPIO) |= 0x08;
 			while(!((*SYSCTL_PRGPIO)|= 0x08));
 			break;
 		
 		case PORTE_AHB:
-		case PORTE_APB:
+		case PORTE:
 			(*SYSCTL_RCGCGPIO) |= 0x10;
 			while(!((*SYSCTL_PRGPIO)|= 0x10));
 			break;
 		
 		case PORTF_AHB:
-		case PORTF_APB:
+		case PORTF:
 			(*SYSCTL_RCGCGPIO) |= 0x20;
 			while(!((*SYSCTL_PRGPIO)|= 0x20));
 			break;
@@ -103,19 +57,190 @@ void gpio_init_pin(volatile uint32_t portAddress, Pin pin, PinMode mode, PinDir 
 	}else if(dir == INPUT){
 		Memory(portAddress, GPIO_DIR) &= ~(pin);
 		Memory(portAddress, GPIO_AFSEL) &= ~(pin);
-	}else{
+	}else if(dir==PERIPHERAL)
+	{
 		Memory(portAddress, GPIO_AFSEL) |= (pin);
 	}
 	
 }
 
 
-void gpio_set_pullup(volatile uint32_t portAddress, Pin pin){
+uint8_t GPIO_readPin(volatile uint32_t portAddress,Pin pin){
+	return Memory(portAddress, PORT_OFFSET) & pin;
+}
+void GPIO_writePin(volatile uint32_t portAddress, Pin pin, uint8_t value){
+	Memory(portAddress, (pin<<2)) = value;
+}
+
+void GPIO_setPullup(volatile uint32_t portAddress, Pin pin){
 	Memory(portAddress, GPIO_PUR) |= pin;
 }
-void gpio_set_pulldown(volatile uint32_t portAddress, Pin pin){
+void GPIO_setPulldown(volatile uint32_t portAddress, Pin pin){
 	Memory(portAddress, GPIO_PDR) |= pin;
 }
+
+
+void GPIO_initInterrupt(volatile uint32_t portAddress, Pin pin, InterruptEvent ev, void (*callback)(int)){
+	switch(portAddress){
+		case PORTA_AHB:
+		case PORTA:
+			callbackTable[0][Log(pin)] = callback;
+			break;
+		
+		case PORTB_AHB:
+		case PORTB:
+			callbackTable[1][Log(pin)] = callback;
+			break;
+		
+		case PORTC_AHB:
+		case PORTC:
+			callbackTable[2][Log(pin)] = callback;
+			break;
+		
+		case PORTD_AHB:
+		case PORTD:
+			callbackTable[3][Log(pin)] = callback;
+			break;
+		
+		case PORTE_AHB:
+		case PORTE:
+			callbackTable[4][Log(pin)] = callback;
+			break;
+		
+		case PORTF_AHB:
+		case PORTF:
+			callbackTable[5][Log(pin)] = callback;
+			break;
+	}
+	Memory(portAddress, GPIO_IS) &= ~(pin);
+	if(ev == BOTH){
+		Memory(portAddress, GPIO_IBE) |= (pin);
+	}else if(ev == FALLING){
+		Memory(portAddress, GPIO_IBE) &= ~(pin);
+		Memory(portAddress, GPIO_IEV) &= ~(pin);
+	}else if(ev == RISING){
+		Memory(portAddress, GPIO_IBE) &= ~(pin);
+		Memory(portAddress, GPIO_IEV) |= (pin);
+	}
+	
+	Memory(portAddress, GPIO_ICR) |= (pin);
+	Memory(portAddress, GPIO_IM)  |= (pin);
+	switch(portAddress){
+		case PORTA:	NVIC_EN0_R |= (1 << 0); break;
+		case PORTB:	NVIC_EN0_R |= (1 << 1); break;
+		case PORTC:	NVIC_EN0_R |= (1 << 2); break;
+		case PORTD:	NVIC_EN0_R |= (1 << 3); break;
+		case PORTE:	NVIC_EN0_R |= (1 << 4);	break;
+		case PORTF: NVIC_EN0_R |= (1 << 30);break;
+	}
+}
+
+void GPIOF_Handler(){
+	if(Memory(PORTF,GPIO_MIS) == PIN0){
+		Memory(PORTF,GPIO_ICR) |= PIN0;
+		callbackTable[5][0]();
+	}
+	else if(Memory(PORTF,GPIO_MIS) == PIN1){
+		Memory(PORTF,GPIO_ICR) |= PIN1;
+		callbackTable[5][1]();
+	}
+	else if(Memory(PORTF,GPIO_MIS) == PIN2){
+		Memory(PORTF,GPIO_ICR) |= PIN2;
+		callbackTable[5][2]();
+	}
+	else if(Memory(PORTF,GPIO_MIS) == PIN3){
+		Memory(PORTF,GPIO_ICR) |= PIN3;
+		callbackTable[5][3]();
+	}
+	else if(Memory(PORTF,GPIO_MIS) == PIN4){
+		Memory(PORTF,GPIO_ICR) |= PIN4;
+		callbackTable[5][4]();
+	}
+	else if(Memory(PORTF,GPIO_MIS) == PIN5){
+		Memory(PORTF,GPIO_ICR) |= PIN5;
+		callbackTable[5][5]();
+	}
+	else if(Memory(PORTF,GPIO_MIS) == PIN6){
+		Memory(PORTF,GPIO_ICR) |= PIN6;
+		callbackTable[5][6]();
+	}
+	else if(Memory(PORTF,GPIO_MIS) == PIN7){
+		Memory(PORTF,GPIO_ICR) |= PIN7;
+		callbackTable[5][7]();
+	}
+}
+
+
+void GPIO_setPCTL(volatile uint32_t portAddress, Pin pin, uint8_t function){
+	Memory(portAddress, GPIO_PCTL) &= ~(0x0f << (pin << 2));
+	Memory(portAddress, GPIO_PCTL) |= (function << (pin << 2));
+}
+void GPIO_setOpenDrain(volatile uint32_t portAddress, Pin pin){
+	Memory(portAddress, GPIO_ODR) |= pin;
+}
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+void gpio_enable_Interrupts(void){
+	__asm__("CPSIE  I \n");
+}
+
+void GPIO_initPort(volatile uint32_t portAdress, PinDir dir){
+	switch(portAdress){
+		case PORTA_AHB:
+		case PORTA:
+			(*SYSCTL_RCGCGPIO) |= 0x01;
+			while(!((*SYSCTL_PRGPIO)|= 0x01));
+			break;
+		
+		case PORTB_AHB:
+		case PORTB:
+			(*SYSCTL_RCGCGPIO) |= 0x02;
+			while(!((*SYSCTL_PRGPIO)|= 0x02));
+			break;
+		
+		case PORTC_AHB:
+		case PORTC:
+			(*SYSCTL_RCGCGPIO) |= 0x04;
+			while(!((*SYSCTL_PRGPIO)|= 0x04));
+			break;
+		
+		case PORTD_AHB:
+		case PORTD:
+			(*SYSCTL_RCGCGPIO) |= 0x08;
+			while(!((*SYSCTL_PRGPIO)|= 0x08));
+			break;
+		
+		case PORTE_AHB:
+		case PORTE:
+			(*SYSCTL_RCGCGPIO) |= 0x10;
+			while(!((*SYSCTL_PRGPIO)|= 0x10));
+			break;
+		
+		case PORTF_AHB:
+		case PORTF:
+			(*SYSCTL_RCGCGPIO) |= 0x20;
+			while(!((*SYSCTL_PRGPIO)|= 0x20));
+			break;
+	}
+	Memory(portAdress, GPIO_LCK) 		= UNLOCK;
+	Memory(portAdress, GPIO_CR) 		= 0xFF;
+	Memory(portAdress, GPIO_DEN) 		= 0xFF;
+	Memory(portAdress, GPIO_AMSEL)	= 0x00;
+	Memory(portAdress, GPIO_AFSEL) 	= 0x00;
+	Memory(portAdress, GPIO_PCTL) 	= 0x00;
+	Memory(portAdress, GPIO_DIR) 		= dir;
+}
+
 void gpio_set_drive2m(volatile uint32_t portAddress, Pin pin){
 	Memory(portAddress, GPIO_DRV2M) |= pin;
 }
@@ -135,17 +260,9 @@ void gpio_clear_register(volatile uint32_t portAddress, Pin pin, RegistersOffset
 }
 
 
-uint8_t gpio_read_port(volatile uint32_t portAddress){
+uint8_t GPIO_readPort(volatile uint32_t portAddress){
 	return Memory(portAddress, PORT_OFFSET);
 }
-void gpio_write_port(volatile uint32_t portAddress, uint8_t value){
+void GPIO_writePort(volatile uint32_t portAddress, uint8_t value){
 	Memory(portAddress, PORT_OFFSET) = value;
-}
-
-
-uint8_t gpio_read_pin(volatile uint32_t portAddress,Pin pin){
-	return Memory(portAddress, PORT_OFFSET) & pin;
-}
-void gpio_write_pin(volatile uint32_t portAddress, PinOffset pin, uint8_t value){
-	Memory(portAddress, pin) = value;
 }
--- ARM-TM4C123/GPIO_Driver/gpio.h
+++ ARM-TM4C123/GPIO_Driver/gpio.h
@@ -1,19 +1,22 @@
 #ifndef _GPIO_DRIVER_H
 #define _GPIO_DRIVER_H
 
-#include <stdint.h>
 
-#define PORTA_APB				0x40004000
+#include "C:\Keil_v5\EE319Kware\inc\tm4c123gh6pm.h"
+#include <stdint.h>
+#include <math.h>
+
+#define PORTA						0x40004000
 #define PORTA_AHB				0x40058000
-#define PORTB_APB				0x40005000
+#define PORTB						0x40005000
 #define PORTB_AHB				0x40059000
-#define PORTC_APB				0x40006000
+#define PORTC						0x40006000
 #define PORTC_AHB				0x4005A000
-#define PORTD_APB				0x40007000
+#define PORTD						0x40007000
 #define PORTD_AHB				0x4005B000
-#define PORTE_APB				0x40024000
+#define PORTE						0x40024000
 #define PORTE_AHB				0x4005C000
-#define PORTF_APB				0x40025000
+#define PORTF						0x40025000
 #define PORTF_AHB				0x4005D000
 	
 #define SYSCTL_PRGPIO   ((volatile uint32_t*)0x400FEA08)
@@ -25,9 +28,20 @@
 #define LOW							0x00
 
 #define Memory(X,Y)			(*((volatile uint32_t*)(((volatile uint32_t)X)+((volatile uint32_t)Y))))
+#define Log(X)					(int)log2(X)
+
+#define I2C_F						0x03
+//#define NVIC_EN0 				0xE000E100
 
 typedef enum RegistersOffset{
 	GPIO_DIR 			= 0x400,
+	GPIO_IS 			= 0x404,
+	GPIO_IBE 			= 0x408,
+	GPIO_IEV 			= 0x40C,
+	GPIO_IM 			= 0x410,
+	GPIO_RIS			= 0x414,
+	GPIO_MIS			= 0x418,
+	GPIO_ICR			= 0x41C,
 	GPIO_AFSEL		=	0x420,
 	GPIO_DRV2M		=	0x500,
 	GPIO_DRV4M		=	0x504,
@@ -44,14 +58,14 @@ typedef enum RegistersOffset{
 } RegistersOffset;
 
 typedef enum Pin{
-	PIN0					=	0x01,
-	PIN1					=	0x02,
-	PIN2					=	0x04,
-	PIN3					=	0x08,
-	PIN4					=	0x10,
-	PIN5					=	0x20,
-	PIN6					=	0x40,
-	PIN7					=	0x80,
+	PIN0					=	0x001,
+	PIN1					=	0x002,
+	PIN2					=	0x004,
+	PIN3					=	0x008,
+	PIN4					=	0x010,
+	PIN5					=	0x020,
+	PIN6					=	0x040,
+	PIN7					=	0x080,
 } Pin;
 
 typedef enum PinOffset{
@@ -76,23 +90,49 @@ typedef enum PinDir{
 	PERIPHERAL,
 } PinDir;
 
-void 		gpio_init_port(volatile uint32_t portAddress, PinDir dir);
-void 		gpio_init_pin(volatile uint32_t portAddress, Pin pin, PinMode mode, PinDir dir);
+typedef enum InterruptEvent{
+	RISING,
+	FALLING,
+	BOTH,
+} InterruptEvent;
 
-void 		gpio_set_pullup(volatile uint32_t portAddress, Pin pin);
-void 		gpio_set_pulldown(volatile uint32_t portAddress, Pin pin);
+typedef enum InterruptPin{
+	IPIN0 = 0,
+	IPIN1 = 1,
+	IPIN2 = 2,
+	IPIN3 = 3,
+	IPIN4 = 4,
+	IPIN5 = 5,
+	IPIN6 = 6,
+	IPIN7 = 7,
+} InterruptPin;
 
-uint8_t gpio_read_pin(volatile uint32_t portAddress,Pin pin);
-void 		gpio_write_pin(volatile uint32_t portAddress, PinOffset pin, uint8_t value);
 
-void 		gpio_set_register(volatile uint32_t portAddress, Pin pin, RegistersOffset registerName);
-void 		gpio_clear_register(volatile uint32_t portAddress, Pin pin, RegistersOffset registerName);
+void 		GPIO_initPin(volatile uint32_t portAddress, Pin pin, PinMode mode, PinDir dir);
 
-uint8_t gpio_read_port(volatile uint32_t portAddress);
-void    gpio_write_port(volatile uint32_t portAddress, uint8_t value);
+uint8_t GPIO_readPin(volatile uint32_t portAddress,Pin pin);
+void 		GPIO_writePin(volatile uint32_t portAddress, Pin pin, uint8_t value);
 
+void GPIO_initInterrupt(volatile uint32_t portAddress, Pin pin, InterruptEvent ev, void (*callback)(int));
+
+void 		GPIO_setPullup(volatile uint32_t portAddress, Pin pin);
+void 		GPIO_setPulldown(volatile uint32_t portAddress, Pin pin);
+void 		GPIO_setPCTL(volatile uint32_t portAddress, Pin pin, uint8_t function);
+void 		GPIO_setOpenDrain(volatile uint32_t portAddress, Pin pin);
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+void 		gpio_enable_Interrupts(void);
+void 		GPIO_initPort(volatile uint32_t portAddress, PinDir dir);
+uint8_t GPIO_readPort(volatile uint32_t portAddress);
+void    GPIO_writePort(volatile uint32_t portAddress, uint8_t value);
 void 		gpio_set_drive2m(volatile uint32_t portAddress, Pin pin);
 void 		gpio_set_drive4m(volatile uint32_t portAddress, Pin pin);
 void 		gpio_set_drive8m(volatile uint32_t portAddress, Pin pin);
+void 		gpio_set_register(volatile uint32_t portAddress, Pin pin, RegistersOffset registerName);
+void 		gpio_clear_register(volatile uint32_t portAddress, Pin pin, RegistersOffset registerName);
 
 #endif
